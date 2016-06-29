fda3d06f116c37bc798f2ede98f96304315ef889
Conrad G Bailey <grhmbailey@gmail.com>
2016-06-29 13:59:37 -0600

    running a hardcoded Amazon search is up

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var parser = require('xml2json');
+var request = require('request');
+var amazon = require('amazon-product-api');
 
+var client = amazon.createClient({
+  awsId: "AKIAJS5FBMNWMU3XMK7Q",
+  awsSecret: "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",
+  awsTag: "markitcapston-20"
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.itemSearch({
+    searchIndex: 'Books',
+    keywords: 'rpg games',
+    responseGroup: 'ItemAttributes,Offers,Images,Large'
+  }).then(function(results){
+    console.log(results);
+    res.send(results)
+  }).catch(function(err){
+    console.log(err);
+  });
 });
-
 module.exports = router;
